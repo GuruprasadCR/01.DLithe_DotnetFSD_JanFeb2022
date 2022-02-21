0d145ed9c9d86cbe4e75520ef47b76760681d0ff
Guru prasad C R <98510032+GuruprasadCR@users.noreply.github.com>
2022-02-21 09:36:33 +0530

    Add files via upload

---
+++ Guruprasad C R/01. Front End/C#/Task1/Largest of 3.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P4
+{
+    class LargestOf3
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Please enter three Numbers");
+            string s1 = Console.ReadLine();
+            string s2 = Console.ReadLine();
+            string s3 = Console.ReadLine();
+            int N1 = Convert.ToInt32(s1);
+            int N2 = Convert.ToInt32(s2);
+            int N3 = Convert.ToInt32(s3);
+            if (N1 > N2)
+            {
+                if (N1 > N3)
+                {
+                    Console.WriteLine("Largest Number is: " + N1);
+                }
+                else
+                {
+                    Console.WriteLine("Largest Number is: " + N3);
+                }
+            }
+            else
+            {
+                if (N2 > N3)
+                {
+                    Console.WriteLine("Largest Number is: " + N2);
+                }
+                else
+                {
+                    Console.WriteLine("Largest number is: " + N3);
+                }
+            }
+
+        }
+    }
+}
