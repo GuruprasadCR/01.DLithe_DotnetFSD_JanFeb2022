ac23c9aff10efeab41364a1d56a537c177078b41
Guru prasad C R <98510032+GuruprasadCR@users.noreply.github.com>
2022-03-01 11:48:53 +0530

    Add files via upload

---
+++ Guruprasad C R/01. Front End/C#/Task 32/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;  // include the System.IO namespace
+
+namespace Files
+{
+    class Fileswrite
+    {
+        static void Main(string[] args)
+        {
+            string writeText = "Hai. How are you?";  // Create a text string
+            File.WriteAllText("Hello2.txt", writeText);  // Create a file and write the contents of writeText to it
+
+            string readText = File.ReadAllText("Hello2.txt"); // Read the contents of the file
+            Console.WriteLine(readText); // Output the content
+        }
+    }
+}
---
+++ Guruprasad C R/01. Front End/C#/Task 32/Files.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;  // include the System.IO namespace
+
+namespace Files
+{
+    class FilesRead
+    {
+        static void Main(string[] args)
+
+        {
+            //file read
+            string path = @"C:\Users\Guruprasad\Desktop\Guruprasad C R\03.C#\Task 32\Hello.txt";
+            string lines;
+
+            lines = File.ReadAllText(path);
+            Console.WriteLine(lines);
+            Console.ReadKey();
+
+        }
+    }
+}
