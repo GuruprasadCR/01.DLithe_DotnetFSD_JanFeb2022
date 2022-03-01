c651fac1bc9aa43c6d4e561399b1ca30bb3627af
Guru prasad C R <98510032+GuruprasadCR@users.noreply.github.com>
2022-03-01 11:52:11 +0530

    Add files via upload

---
+++ Guruprasad C R/01. Front End/C#/Debugging 3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace InterfaceDemo
+{
+    interface interface1
+       {
+        void Test();
+        void Show();
+        }
+
+    interface interface2
+      {
+        void Test();
+        void Show();
+      }
+
+    class ImplementInterafce : interface1, interface2
+    {
+        public void Test()
+        {
+
+            Console.WriteLine("Test method is implemented");
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("Show method is implemented");
+        }
+
+    }
+
+      class Program
+      {
+         static void Main(string[] args)
+        {
+        ImplementInterafce obj = new ImplementInterafce();
+        obj.Test();
+        obj.Show();
+        Console.WriteLine("Press any key to exit");
+        Console.ReadKey();
+          }
+      }
+}
+
