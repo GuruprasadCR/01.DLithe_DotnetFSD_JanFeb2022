11293ba3f74d78c3be33ea453956382625549ad4
Guru prasad C R <98510032+GuruprasadCR@users.noreply.github.com>
2022-03-01 12:17:00 +0530

    Add files via upload

---
+++ Guruprasad C R/01. Front End/C#/Task 25/Encapsulation1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    public class employee
+    {
+
+        // private variables declared
+        // these can only be accessed by
+        // public methods of class
+        private String employeename;
+        private int employeeage;
+
+        // using accessors to get and 
+        // set the value of studentName
+        public String Name
+        {
+
+            get
+            {
+                return employeename;
+            }
+
+            set
+            {
+                employeename = value;
+            }
+
+        }
+
+        // using accessors to get and 
+        // set the value of studentAge
+        public int Age
+        {
+
+            get
+            {
+                return employeeage;
+            }
+
+            set
+            {
+                employeeage = value;
+            }
+        }
+    }
+
+    // Driver Class
+    class Encapsulated
+    {
+
+        // Main Method
+        static public void Main()
+        {
+
+            // creating object
+            employee emp1 = new employee();
+
+            
+            // standard field 'value'
+            emp1.Name = "Rakshith";
+
+            // standard field 'value'
+            emp1.Age = 25;
+
+            // Displaying values of the variables
+            Console.WriteLine("Name: " + emp1.Name);
+            Console.WriteLine("Age: " + emp1.Age);
+        }
+    }
+}
---
+++ Guruprasad C R/01. Front End/C#/Task 25/Inheritance.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace inheritence
+{
+    class company
+    {
+        protected int id;
+        protected string name;
+
+        public void Employeeinfo(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+            Console.WriteLine("Base Employee Id: " + id);
+            Console.WriteLine("Base Employee Name: " + name);
+        }
+    }
+
+    class employee : company 
+    {
+        
+        public void employeeetails()//parameters can also be possible here
+        {
+            Console.WriteLine("Employee Details are");
+            id = 100;
+            name = "Ram";
+            company C = new company();
+            C.Employeeinfo(id, name);
+            Console.WriteLine("Derived Employee Id: " + id);
+            Console.WriteLine("Derived Employee Name: " + name);
+        }
+    }
+
+    class Main_Class
+    {
+        static void Main(String[] args)
+        {
+            employee emp1 = new employee();
+            emp1.employeeetails();
+        }
+    }
+}
