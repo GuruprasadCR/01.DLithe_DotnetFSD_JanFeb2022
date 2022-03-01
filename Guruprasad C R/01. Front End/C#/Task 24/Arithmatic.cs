a26337a2d9b94cf1223e01c911b15e1b5d246704
Guru prasad C R <98510032+GuruprasadCR@users.noreply.github.com>
2022-03-01 12:45:46 +0530

    String,Arithmetic

---
+++ Guruprasad C R/01. Front End/C#/Task 24/Arithmatic.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace P2
+{
+    class Airthmatic
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Airthmatic Operations");
+            int a = 100;
+            int b = 20;
+            Console.WriteLine("The value of a= "+a);
+            Console.WriteLine("The value of b= " + b);
+            Console.WriteLine("Addition a + b= " + (a + b));
+            Console.WriteLine("Substraction a - b= " + (a - b));
+            Console.WriteLine("Multiplication a * b= " + (a * b));
+            Console.WriteLine("Division a / b= " + (a / b));
+            Console.WriteLine("Modulus a % b= " + (a % b));
+        }
+    }
+}
---
+++ Guruprasad C R/01. Front End/C#/Task 24/Swap.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P3
+{
+        class Swap
+        {
+            static void Main(String[] args)
+            {
+                Console.WriteLine("Enter the first number A: ");
+                string a = Console.ReadLine();
+                int Num1 = Convert.ToInt32(a);  //100
+                Console.WriteLine("Enter the first number B: ");
+                string b = Console.ReadLine();
+                int Num2 = Convert.ToInt32(b);  //50
+                Console.WriteLine("Before Swap:");
+                Console.WriteLine("A: " + Num1);
+                Console.WriteLine("B: " + Num2);
+                Num1 = Num1 + Num2;            //150
+                Num2 = Num1 - Num2;           //50
+                Num1 = Num1 - Num2;           //100
+                Console.WriteLine("After Swap:");
+                Console.WriteLine("A: " + Num1);
+                Console.WriteLine("B: " + Num2);
+            }
+
+
+        }
+}
+
+
+
+
